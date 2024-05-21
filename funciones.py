7f7045972655acbc9f1a56417145739a90018228
Juan Sebastian Maldonado Gomez <107078002+magneticjuan@users.noreply.github.com>
2024-05-21 16:27:00 -0500

    Cambios al update

--- funciones.py
+++ funciones.py
@@ -2,25 +2,13 @@ import numpy as np
 import pandas as pd
 
 def identificar_outliers(df, columna):
-    """
-    Identifica los outliers en una columna de un DataFrame.
-
-    Un outlier se define como un valor que se desvía más de tres desviaciones estándar de la media.
-
-    Args:
-    df (pd.DataFrame): El DataFrame que contiene los datos.
-    columna (str): El nombre de la columna en la que se deben identificar los outliers.
-
-    Returns:
-    list: Una lista de índices de las filas que contienen outliers.
-    """
     media = df[columna].mean()
     desv_std = df[columna].std()
     umbral = 3
-
-    outliers = np.abs(df[columna] - media) > (umbral * desv_std)
-
-    return df[outliers].index.tolist()
+    outliers_std = np.abs(df[columna] - media) > (umbral * desv_std)
+    return {
+        'outliers_std': df[outliers_std].index.tolist()
+    }
 
 def porcentaje_nulos(df):
     """
--- sf_to_csv.py
+++ sf_to_csv.py
@@ -11,38 +11,39 @@ USERNAME = getenv('USERNAME')
 PASSWORD = getenv('PASSWORD')
 SECURITY_TOKEN = getenv('SECURITY_TOKEN')
 
-try:
-    # Autenticarse en Salesforce
-    sf = Salesforce(username=USERNAME, password=PASSWORD, security_token=SECURITY_TOKEN)
-    print('Conectado a Salesforce exitosamente.')
-    
-    # Definir los parámetros de la consulta
-    columna_fecha = 'CreatedDate'
-    fecha_corte = '2024-01-01T00:00:00Z'
-    objeto = 'Lead'
-    campo_1 = 'Cuota_Inicial__C'
-    campo_2 = 'Presupuesto_inmueble_a_comprar__c'
-    campo_3 = 'ingresos_totales__c'
-    campo_4 = 'Ingresos_familiares__c'
-    campo_5 = 'Calidad_de_lead__c'
-    campo_6 = 'picklist_ciudad__c'
-    
-    # Formar la consulta SOQL
-    query = f"SELECT {campo_1}, {campo_2}, {campo_3}, {campo_4}, {campo_5}, {campo_6} FROM {objeto} WHERE {columna_fecha} > {fecha_corte}"
-    result = sf.query(query)
-    print('Consulta realizada correctamente.')
-    
-    # Convertir el resultado de la consulta en un DataFrame de pandas
-    df = pd.DataFrame(result['records'])
-    
-    # Eliminar la columna 'attributes' si existe
-    if 'attributes' in df.columns:
-        df = df.drop(columns='attributes')
-    
-    # Guardar el DataFrame en un archivo CSV
-    df.to_csv('Datos_Train_Calidad_Lead.csv', index=False)
-    print('Datos guardados en "Datos_Train_Calidad_Lead.csv".')
+def get_data_sf ():
+    try:
+        # Autenticarse en Salesforce
+        sf = Salesforce(username=USERNAME, password=PASSWORD, security_token=SECURITY_TOKEN)
+        print('Conectado a Salesforce exitosamente.')
+        
+        # Definir los parámetros de la consulta
+        columna_fecha = 'CreatedDate'
+        fecha_corte = '2024-01-01T00:00:00Z'
+        objeto = 'Lead'
+        campo_1 = 'Cuota_Inicial__C'
+        campo_2 = 'Presupuesto_inmueble_a_comprar__c'
+        campo_3 = 'ingresos_totales__c'
+        campo_4 = 'Ingresos_familiares__c'
+        campo_5 = 'Calidad_de_lead__c'
+        campo_6 = 'picklist_ciudad__c'
+        
+        # Formar la consulta SOQL
+        query = f"SELECT {campo_1}, {campo_2}, {campo_3}, {campo_4}, {campo_5}, {campo_6} FROM {objeto} WHERE {columna_fecha} > {fecha_corte}"
+        result = sf.query(query)
+        print('Consulta realizada correctamente.')
+        
+        # Convertir el resultado de la consulta en un DataFrame de pandas
+        df = pd.DataFrame(result['records'])
+        
+        # Eliminar la columna 'attributes' si existe
+        if 'attributes' in df.columns:
+            df = df.drop(columns='attributes')
+        
+        # Guardar el DataFrame en un archivo CSV
+        df.to_csv('Datos_Train_Calidad_Lead.csv', index=False)
+        return 'Datos guardados en "Datos_Train_Calidad_Lead.csv".'
 
-except Exception as e:
-    print("Error conectándose a Salesforce o en la consulta:")
-    print(e)
+    except Exception as e:
+        return f"Error conectándose a Salesforce o en la consulta: {e}"
+        
